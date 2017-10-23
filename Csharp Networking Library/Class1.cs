b3d6f88d40356d56e10d57c37ac776d6882f7feb
Luuk Diederik <flipmil3@hotmail.com>
2017-10-23 23:57:21 +0200

    Creation of

--- Csharp Networking Library/Class1.cs
+++ Csharp Networking Library/Class1.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Csharp_Networking_Library
-{
-    public class Class1
-    {
+namespace Networking {
+    public class Class1 {
     }
 }
